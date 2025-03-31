a8407b4b3bf8258290049a2fd29ea4e2f08c26ef
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-03-31 14:35:39 +0700

    Create Install.ps1

---
+++ Chrome/Install.ps1
@@ -0,0 +1,2 @@
+Invoke-Webrequest -URI https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BBF94584D-3AF6-ED6A-B33E-41E71D889138%7D%26lang%3Den%26browser%3D5%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26brand%3DONGR%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe -Outfile Chrome.exe
+.\Chrome.exe
