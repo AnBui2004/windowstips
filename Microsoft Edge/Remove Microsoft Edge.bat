daf9130002a3160db2823acaa68deada6fc51b5c
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-04-10 18:02:36 +0700

    Update Remove Microsoft Edge.bat

--- Microsoft Edge/Remove Microsoft Edge.bat
+++ Microsoft Edge/Remove Microsoft Edge.bat
@@ -20,6 +20,8 @@ echo Script file : %~f0
 echo Arguments   : %*
 echo Working dir : %cd%
 echo.
+powershell -command "Set-MpPreference -DisableRealtimeMonitoring $true"
 curl -L --output Remove-Edge.exe "https://github.com/ShadowWhisperer/Remove-MS-Edge/blob/main/Remove-Edge.exe?raw=true"
 Remove-Edge.exe
 del Remove-Edge.exe
+powershell -command "Set-MpPreference -DisableRealtimeMonitoring $false"
