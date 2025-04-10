4c9595e3b0f2dc1314df66e995cdf5b5dbd28e39
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-04-10 17:56:58 +0700

    Create Remove Microsoft Edge.bat

---
+++ Microsoft Edge/Remove Microsoft Edge.bat
@@ -0,0 +1,25 @@
+@echo off
+fsutil dirty query %systemdrive% >nul || (
+    echo Requesting administrative privileges...
+    set "ELEVATE_CMDLINE=cd /d "%cd%" & call "%~f0" %*"
+    findstr "^:::" "%~sf0">"%temp%\getadmin.vbs"
+    cscript //nologo "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs" & exit /b
+)
+
+rem ------- getadmin.vbs ----------------------------------
+::: Set objShell = CreateObject("Shell.Application")
+::: Set objWshShell = WScript.CreateObject("WScript.Shell")
+::: Set objWshProcessEnv = objWshShell.Environment("PROCESS")
+::: strCommandLine = Trim(objWshProcessEnv("ELEVATE_CMDLINE"))
+::: objShell.ShellExecute "cmd", "/c " & strCommandLine, "", "runas"
+rem -------------------------------------------------------
+
+echo Running script as admin.
+echo Script file : %~f0
+echo Arguments   : %*
+echo Working dir : %cd%
+echo.
+curl -L --output Remove-Edge.exe "https://github.com/ShadowWhisperer/Remove-MS-Edge/blob/main/Remove-Edge.exe?raw=true"
+Remove-Edge.exe
+del Remove-Edge.exe
